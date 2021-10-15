b4b2db823bdb90849ed85eded6f3cc78ddbe6cac
Obez99 <giuliogx@live.it>
2021-10-15 16:38:59 +0200

    Completes first exercise

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//Inserire un numero
+const numero = parseInt(prompt("Inserire un numero"))
+//Controllare che sia pari
+//Se è pari stamparlo
+if (numero % 2 === 0) {
+  alert(numero)
+} else {
+  //Se è dispari stampare il numero successivo
+  alert(numero + 1);
+}
